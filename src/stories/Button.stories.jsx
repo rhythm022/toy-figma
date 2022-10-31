bc082ad32917da5b5b8eb527c034cbe73dc49615
jiangjj <jiangjj@sse.net.cn>
2022-10-31 08:57:28 +0800

    mdx

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -11,11 +11,9 @@ import {
   PRIMARY_STORY,
 } from '@storybook/addon-docs';
 
-// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
   component: MyButton,
-  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
     onClick: {},
@@ -41,15 +39,11 @@ export default {
   },
 };
 
-// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
 const Template = (args) => ({
-  // Components used in your story `template` are defined in the `components` object
   components: { MyButton },
-  // The story's `args` need to be mapped into the template through the `setup()` method
   setup() {
     return { args };
   },
-  // And then the `args` are bound to your component with `v-bind="args"`
   template: '<my-button v-bind="args" />',
 });
 
