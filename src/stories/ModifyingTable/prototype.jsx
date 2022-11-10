f8f2caadf65fcf48b3597a4e815cdb74c98a361e
jiangjj <jiangjj@sse.net.cn>
2022-11-10 18:06:17 +0800

    add 设计

---
+++ src/stories/ModifyingTable/prototype.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function Prototype() {
+  return (
+    <iframe 
+    width="100%" 
+    height="1200" 
+    class="ne-thirdparty-iframe"
+    data-testid="ne-thirdparty-reader-iframe"
+    sandbox="allow-forms allow-orientation-lock allow-presentation allow-same-origin allow-scripts allow-popups allow-downloads" 
+    frameborder="0"
+    allowfullscreen=""
+    src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2Fr8Ibwbo7I095uZB2dA53v7%2FElement-UI-Kit-2.15.7-(Community)%3Fnode-id%3D27347%253A64011"></iframe>
+  );
+}
