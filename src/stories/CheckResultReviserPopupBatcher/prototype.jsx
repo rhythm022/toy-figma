9939726e555a50f3ccf6cc435722414987c6ccc8
jiangjj <jiangjj@sse.net.cn>
2022-11-22 17:29:11 +0800

    refactor

---
+++ src/stories/CheckResultReviserPopupBatcher/prototype.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s1 from "./proto/default.svg";
+
+export function Prototype() {
+  return (
+  <div>
+    <img src={s1} width="1000" title='null' />
+  </div>
+    );
+}
